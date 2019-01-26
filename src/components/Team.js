4bbd23d0c0b96e7b1c6481f5701986475064aa9e
dgekaa <dgeka_vip@mail.ru>
2019-01-26 19:56:45 +0300

    feat: change block with team

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,25 +1,34 @@
 
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 
 import "./Team.css";
 
 class Team extends React.Component {
 	render(){
-		// console.log()
 		return (
 	 		<div>
 	 			<h3 
 	 				style={{textAlign:'center', margin:"40px 0 20px 0"}}>
-	 				Сайт делали
+	 				Сайт делала команда из пяти разработчиков
 	 			</h3>
 	 			{this.props.data.map((item, index) => 
-	 				<div className="teamContainer">
+	 				<div className="teamContainer" key={index}>
 		 				<img src={item.src}></img>
-			 			<p>Имя и фамилия: {item.name}</p>	 				
-			 			<p>Никнейм: {item.nickname}</p>	 				
-			 			Ссылка на профиль: <a target="_blank" href={item.gitHub}>GitHub</a>	 				
+			 			<p>{item.name}</p>	 				
+			 			<p>{item.nickname}</p>	
+			 			<a 
+			 				target="_blank" 
+			 				href={item.gitHub} 
+			 				style={{
+			 						fontSize:'40px',
+			 						color:'#3d3d3d'
+			 				}}>
+			 				<FontAwesomeIcon icon = {faGithub} /> 
+			 			</a>	 				
 	 				</div>
 				)}
 	 		</div>
