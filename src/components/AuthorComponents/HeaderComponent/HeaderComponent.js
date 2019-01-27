e2b97755a681d108b2b88bd0103a8f1dc75bea30
Antony <A.Krutenok@gmail.com>
2019-01-27 13:40:09 +0300

    style: fix header styles

--- src/components/AuthorComponents/HeaderComponent/HeaderComponent.js
+++ src/components/AuthorComponents/HeaderComponent/HeaderComponent.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './headerStyle.css';
 import { Link } from 'gatsby';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faMapMarked } from '@fortawesome/free-solid-svg-icons';
+import { faTable } from '@fortawesome/free-solid-svg-icons';
+import { faImages } from '@fortawesome/free-solid-svg-icons';
+
 class HeaderComponent extends React.Component {
     constructor(){
         super();
@@ -8,22 +17,13 @@ class HeaderComponent extends React.Component {
     render(){
         return (
         <div className="header">
-            <Link
-          to="/"
-          style={{
-            backgroundColor: 'dodgerblue',  
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          Main page
-        </Link>
             <div className="header-right">
-                <a className="active" href="#timeline">Timeline</a>
-                <a className="active" href="#youtube">Youtube</a>
-                <a className="active" href="#map">Map</a>
-                <a className="active" href="#masterpiece">Masterpieces</a>
-                <a className="active" href="#gallery">Gallery</a>
+                <a className="active" href="/"><FontAwesomeIcon icon = {faHome} /></a>
+                <a className="active" href="#timeline"><FontAwesomeIcon icon = {faCalendarAlt} /></a>
+                <a className="active" href="#youtube"><FontAwesomeIcon icon = {faYoutube} /></a>
+                <a className="active" href="#map"><FontAwesomeIcon icon = {faMapMarked} /> </a>
+                <a className="active" href="#masterpiece"><FontAwesomeIcon icon = {faTable} /></a>
+                <a className="active" href="#gallery"><FontAwesomeIcon icon = {faImages} /></a>
             </div>
         </div>);
     }
