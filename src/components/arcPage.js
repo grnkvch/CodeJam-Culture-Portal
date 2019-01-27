76c80c15a2c6210ac7eac8fcd693652e13f537f4
dgekaa <dgeka_vip@mail.ru>
2019-01-28 02:06:24 +0300

    style: change photo style

--- src/components/arcPage.js
+++ src/components/arcPage.js
@@ -26,7 +26,7 @@ const arcPage = ({data, t}) => {
     <Layout>
       <Fragment>                                                          
         <PageNavigation /> 
-        <div className="image-title" style={{marginTop: '120px'}}><img style={{width:"250px", height:"300px"}} src={image} alt="Author's picture" /></div>
+        <div className="image-title"><img src={image} alt="Author's picture" /></div>
         <div style={{ textAlign: 'center', fontSize: '30px' }}>{arc.name}</div>
         <div style={{ textAlign: 'center', fontSize: '20px' }}>{arc.vita}</div>
         <div id="timeline" className="title" style={{ textAlign: 'center'}}><h1>{t(`arcTimeline`)}</h1></div>
