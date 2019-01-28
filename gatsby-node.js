6bcc1f61c2209305628a15ed749ca3e9bd521376
grnkvch <grinkevich.vv@gmail.com>
2019-01-28 05:41:19 +0300

    fix: resolve page path multipling issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,47 +6,37 @@
 
 // You can delete this file if you're not using it
 const path = require('path');
+
 exports.createPages = ({ actions, graphql }) => {
-    const { createPage } = actions;
-  
-    const arcPage = path.resolve('src/components/arcPage.js');
-  
-    return graphql(`{
+  const { createPage } = actions;
+
+  const arcPage = path.resolve('src/components/arcPage.js');
+
+  return graphql(`{
       allJavascriptFrontmatter {
         edges {
           node {
             frontmatter {
               error
               path
-              name
-              date
-              img
-              videoId
-              timelineData {
-                date
-                text
-              }
-              work{
-                title
-                width
-                length
-                date
-                img
-              }
             }
           }
         }
       }
     }`)
-      .then((res) => {
-        if (res.errors) {
-          return Promise.reject(res.errors);
-        }
-        res.data.allJavascriptFrontmatter.edges.forEach(({ node }) => {
+    .then((res) => {
+      if (res.errors) {
+        return Promise.reject(res.errors);
+      }
+      const hash = {};
+      res.data.allJavascriptFrontmatter.edges.forEach(({ node }) => {
+        if (!hash[node.frontmatter.path]) {
+          hash[node.frontmatter.path] = true;
           createPage({
             path: node.frontmatter.path,
             component: arcPage,
           });
-        });
+        }
       });
-  };
+    });
+};
